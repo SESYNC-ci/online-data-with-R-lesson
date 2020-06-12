634b2a37b819837e1836875ab3127454ddd36ebc
Quentin Read <quentin.read@gmail.com>
2020-06-12 11:39:34 -0400

    Port paging and stashing part of lesson to rsqlite!

--- test_lesson_code.R
+++ test_lesson_code.R
@@ -64,13 +64,106 @@ response = GET(path, query = query_params)
 response
 
 # second code block: determine the content type
+response$headers['content-type']
 
-# third code block: convert JSON to dataframe
+# third code block: convert JSON to matrix
+library(jsonlite)
 
+maryland_income <- response %>%
+  content(as = 'text') %>%
+  fromJSON()
+
+head(maryland_income)
+# Maybe we should note that this doesn't recognize the header row. In "real life" we would convert to a data frame and promote first row to header
 
 # Specialized Packages ----------------------------------------------------
 
 # first code block: ACS5 for 2017 (not sure what this does)
 
-# scond and third code blocks: define what variables to get and pull from census pkg
+# second and third code blocks: define what variables to get and pull from census pkg
 
+
+# Paging and Stashing -----------------------------------------------------
+
+# first code block: read api_key into the environment
+source('api_key.R')
+
+# second code block: query the USDA API for fruit nutrition info
+api <- 'https://api.nal.usda.gov/fdc/v1/'
+path <- 'foods/search'
+query_params <- list('api_key' = API_KEY,
+                     'query' = 'fruit')
+doc <- GET(paste0(api, path), query = query_params) %>%
+  content(as = 'parsed')
+
+# 3rd-5th code blocks : inspect the doc object
+
+names(doc)
+
+doc$totalHits
+
+length(doc$foods)
+
+# code block 6-7: setup database schema
+
+# code block 8: extract one fruit and see its name
+fruit <- doc$foods[[1]]
+fruit$description
+
+# code block 9: list some of the nutrients (with purrr)
+library(purrr)
+library(dplyr)
+
+map_dfr(fruit$foodNutrients, 
+        ~ data.frame(name = .$nutrientName, value = .$value)) %>%
+  head(10)
+
+# code block 10: initialize 
+library(DBI) # RSQLite is a DBI compliant package, whatever that means, and itself contains sqlite
+library(RSQLite)
+
+fruit_db <- dbConnect(SQLite(), 'fruits.sqlite') # Creates in WD
+
+dbExecute(conn = fruit_db,
+            'CREATE TABLE Food
+              (foodID INTEGER,
+               name TEXT,
+               sugar FLOAT)')
+
+# code block 11-12: increase page size parameter and repeatedly page through and stash
+query_params$pageSize <- 100
+
+# This is really long code and probably ends up confusing people. I'd like to find a simpler example.
+get_sugar_content <- function(fruit) {
+  nutrients <- map_dfr(fruit$foodNutrients, ~ data.frame(name = .$nutrientName, value = .$value)) 
+  sugar_content <- nutrients %>% 
+    filter(grepl('Sugar', name)) %>%
+    pull(value)
+  ifelse(length(sugar_content) == 1, sugar_content, as.numeric(NA))
+}
+
+for (i in 1:10) {
+  # Advance page and query
+  query_params$pageNumber <- i
+  response <- GET(paste0(api, path), query = query_params) 
+  page <- content(response, as = 'parsed')
+  fruits <- page$foods
+  
+  # Save page 
+  values <- map_dfr(fruits, 
+                    ~ data.frame(foodID = .$fdcId,
+                                 name = .$description,
+                                 sugar = get_sugar_content(.)))
+  
+  # Stash in database
+  dbWriteTable(conn = fruit_db, name = 'Food', value = values, append = TRUE)
+
+}
+
+# code block 13: read to dataframe and view
+
+fruit_sugar_content <- dbReadTable(conn = fruit_db, name = 'Food')
+head(fruit_sugar_content, 10)
+
+# code block 14: disconnect from database
+dbDisconnect(conn = fruit_db)
